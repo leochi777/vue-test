415328ad7042399d2c6e5b64fe844ae83776f184
leochi777 <tzuchi0822@gmail.com>
2021-02-23 11:25:44 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="yoooooooooooo"/>
+    <HelloWorld msg="yoooooooodddoooo"/>
   </div>
 </template>
 
